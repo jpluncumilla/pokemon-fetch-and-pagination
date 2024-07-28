1169ff6aef2cd87116203a220dc683168184ca7f
Juan Pablo Luncumilla <jpluncumilla@Juans-MacBook-Pro-2.local>
2024-07-28 17:29:56 -0400

    refactor of function to store response in variable and testing it

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -7,12 +7,23 @@ const PokemonList = () => {
   const [pages, setPages] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
-  function fetchPokemon() {
-    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-      .then((response) => response.json())
-      .then((poke) => console.log(poke))
-      .catch((error) => console.error("Error fetching Pokémon:", error));
-  }
+  //   function fetchPokemon() {
+  //     // offset param for pagination example = ?limit=60&offset=60
+  //     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  //       .then((response) => response.json())
+  //       .then((poke) => console.log(poke))
+  //       .catch((error) => console.error("Error fetching Pokémon:", error));
+  //   }
+
+  const fetchPokemon = async () => {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const data = await response.json();
+    try {
+      console.log(data.results);
+    } catch (err) {
+      console.log("Error fetching Pokémon:", err);
+    }
+  };
 
   useEffect(() => {
     fetchPokemon();
